e2a218951f3feede03a89bae111927d1e76d4af0
Shresthhooda <shooda@ucsd.edu>
2022-04-15 19:05:04 -0700

    Infinite Loop
    We had a test result with that produced an infinite loop with a bracket and a parenthesis in the link.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,18 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        
         while(currentIndex < markdown.length()) {
+            System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
         }
-
+       
         return toReturn;
     }
 
@@ -29,5 +32,6 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+       
     }
 }
