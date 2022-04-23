32b6613747cb82da9b5547168e8b814f61fa6559
KevinZheng2222 <kezheng@ucsd.edu>
2022-04-22 19:28:52 -0700

    MarkdownParseTest
    Running test on test file 1.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,25 +13,24 @@ public class MarkdownParse {
         int currentIndex = 0;
         
         while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
-            if (markdown.indexOf(" ",closeBracket) < markdown.indexOf("(", closeBracket)){
+            /* if (markdown.indexOf(" ",closeBracket) < markdown.indexOf("(", closeBracket)){
                 // System.out.println("hi");
                 int closeParen = markdown.indexOf(")", closeBracket);
                 currentIndex = closeParen + 1;
                 continue;
-            }
+            } */
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(markdown.substring(openParen, openParen + 1).equals("("))
+            /* if(markdown.substring(openParen, openParen + 1).equals("("))
             {
-                toReturn.add(markdown.substring(openParen + 2, closeParen));
+             toReturn.add(markdown.substring(openParen + 2, closeParen));
             }
             else
-            {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-            }
+            { */
+                 toReturn.add(markdown.substring(openParen + 1, closeParen));
+            //}
             
             currentIndex = closeParen + 1;
         }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,24 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void getLinksEquals() throws Exception{
+        ArrayList<String> toReturn = new ArrayList<String>();
+        toReturn.add("https://something.com");
+        toReturn.add("some-thing.html");
+        System.out.println(toReturn);
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(toReturn, links);
     }
 }
